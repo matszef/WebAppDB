5e85de60ec944a949e63faf7dc0fc0a287f0fa41
Synt4x <szefler.mateusz91@gmail.com>
2022-03-18 18:27:46 +0100

    Add project files.

---
+++ WebAppTwo.Infrastructure/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAppTwo.Infrastructure.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ WebAppTwo.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace WebAppTwo.Infrastructure
+{
+    public class UserRepository
+    {
+        SqlMapper sql = "select * from Users";'
+
+        List<Product> products = new 
+
+    }
+}
