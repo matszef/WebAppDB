0236c4bef7d6b041bd1be6f3fea4f4bffb17dc68
dasad44 <dasad33_79@o2.pl>
2022-03-18 19:27:27 +0100

    Code refactore

--- WebAppTwo.Infrastructure/Repositories/UserRepository.cs
+++ WebAppTwo.Infrastructure/Repositories/UserRepository.cs
@@ -11,53 +11,34 @@ namespace WebAppTwo.Infrastructure
 {
     public class UserRepository
     {
-        public void DatabaseConnection()
+        private readonly string _connectionString;
+        public UserRepository()
         {
-            var sql = "select * from Users";
-            var users = new List<User>();
-
-            string connString = "Server=DESKTOP-AGBAN3N\\TEW_SQLEXPRESS;Database=UsersDatabase;Trusted_Connection=True;";
-
-            using (var connection = new SqlConnection(connString))
-            {
-                connection.Open();
-                using (var command = new SqlCommand(sql, connection))
-                {
-                    using (var reader = command.ExecuteReader())
-                    {
-                        var user = new User
-                        {
-                            UserId = reader.GetInt32(reader.GetOrdinal("UserID")),
-                            FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
-                            LastName = reader.GetString(reader.GetOrdinal("LastName")),
-                            EmailAddress = reader.GetString(reader.GetOrdinal("EmailAddress")),
-                            City = reader.GetString(reader.GetOrdinal("City")),
-                        };
-                        users.Add(user);
-                    }
-                }
-            }
+            _connectionString = "Server=DESKTOP-AGBAN3N\\TEW_SQLEXPRESS;Database=UsersDatabase;Trusted_Connection=True;";
         }
 
-        public void DatabaseInsert()
+        public IEnumerable<User> GetUsers()
         {
-            string connString = "Server=DESKTOP-AGBAN3N\\TEW_SQLEXPRESS;Database=UsersDatabase;Trusted_Connection=True;";
+            var sql = "select * from Users";
+            var connection = new SqlConnection(_connectionString);
+            var results = connection.Query<User>(sql, connection);
 
-            using (var connection = new SqlConnection(connString))
+            return results;
+        }
+
+        public void AddUser(User user)
+        {
+            using (var connection = new SqlConnection(_connectionString))
             {
                 connection.Open();
-                var sqlStatement = @"INSERT INTO Users
-                    (UserId, FirstName, LastName, EmailAddress, City)
-                    VALUES (1, 'Mateusz', 'Szefler', 'SSS@SSS.COM', 'Ciechanow')";
+                var sqlStatement = $"INSERT INTO Users (UserId, FirstName, LastName, EmailAddress, City) VALUES (1, 'Mateusz', 'Szefler', 'SSS@SSS.COM', 'Ciechanow')";
                 connection.Execute(sqlStatement, connection);
             }
         }
 
         public void DatabaseDelete(int id)
         {
-            string connString = "Server=DESKTOP-AGBAN3N\\TEW_SQLEXPRESS;Database=UsersDatabase;Trusted_Connection=True;";
-
-            using (var connection = new SqlConnection(connString))
+            using (var connection = new SqlConnection(_connectionString))
             {
                 connection.Open();
                 var sqlStatement = "DELETE Users WHERE UserId = @Id";
